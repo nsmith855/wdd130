e2dd8440e12c280efa95b7293bf3d1f563a2914d
nsmith855 <nsmith855@gmail.com>
2023-09-30 13:38:22 -0600

    added a home page

---
+++ wwr/styles/practice w3.py
@@ -0,0 +1,7 @@
+age=int(input('How old are you?:'))
+#print(f'On your next birthday you will be + {age + 1}')
+#egg=int(input('How many egg cartons do you have?:'))
+#print(f'You have  + {egg*12} eggs.')
+#cookies=int(input('How many cookies do you have?:'))
+#people=int(input('How many people are there?:'))
+#print(f'Each person gets {people/cookies} cookies')
